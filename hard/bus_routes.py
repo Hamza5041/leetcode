7361593de55da6d7800c809b199f21f2f06dcdf4
Hamza Ansari <hamzaans@buffalo.edu>
2024-12-29 16:17:02 -0500

    did a bfs implementation, have 18/54 passing

--- hard/bus_routes.py
+++ hard/bus_routes.py
@@ -1,10 +1,38 @@
+from queue import Queue
 from typing import List
-
 
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
 
-        pass
+        adjList = self.route_graph(routes)
+
+        queue = Queue()
+        visited = set()
+
+        # add all the routes connected to the source from the adj list
+        for route in adjList.get(source):
+            queue.put(route)
+            visited.add(route)
+
+        buses = 1
+        # BFS
+        while not queue.empty():
+            # deque the current route ready in the queue
+            currentRoute = queue.get()
+            # check the routes array for stops. If the stop is the target, return the buses it took to get there
+            for stop in routes[currentRoute]:
+                if stop == target:
+                    return buses
+                # otherwise, queue all the routes for each stop into the queue and mark as visited if they have not been
+                for nextRoute in adjList.get(stop):
+                    if nextRoute not in visited:
+                        visited.add(nextRoute)
+                        queue.put(nextRoute)
+            # increment buses taken at each iteration
+            buses += 1
+        return -1
+
+
 
     def route_graph(self, routes: List[List[int]]) :
         adjList = {}
@@ -27,7 +55,7 @@ if __name__ == '__main__':
 
     solution = Solution()
     result = solution.numBusesToDestination(routes, source, target)
-    # assert result == 2, "Output was not 2, it was: " + str(result)
+    assert result == 2, "Output was not 2, it was: " + str(result)
 
     adjList = solution.route_graph(routes)
     print(adjList)
