6433f0bc15efe5efe7c9eaa24e1e589b93ad9877
Hamza Ansari <hamzaans@buffalo.edu>
2024-12-29 15:58:52 -0500

    changed to just adjList that maps stops -> routes with that stop

--- hard/bus_routes.py
+++ hard/bus_routes.py
@@ -3,27 +3,17 @@ from typing import List
 
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+
         pass
 
     def route_graph(self, routes: List[List[int]]) :
-
-        # map stops to route
-        # map which stop belongs to which bus route. This way you can find routes with shared buses
-        stopsToRoutes = {}
+        adjList = {}
         for i, route in enumerate(routes):
             for stop in route:
-                if stop not in stopsToRoutes:
-                    stopsToRoutes[stop] = set()
-                stopsToRoutes[stop].add(i)
-
-        # build connectingRoutes
-        connectingRoutes = {}
-        for stop, routes_at_stop in stopsToRoutes.items():
-            for route1 in routes_at_stop:
-                if route1 not in connectingRoutes:
-                    connectingRoutes[route1] = set()
-                connectingRoutes[route1].update(routes_at_stop - {route1})
-        return connectingRoutes, stopsToRoutes
+                if stop not in adjList:
+                    adjList[stop] = set()
+                adjList[stop].add(i)
+        return adjList
 
 
 
@@ -39,6 +29,5 @@ if __name__ == '__main__':
     result = solution.numBusesToDestination(routes, source, target)
     # assert result == 2, "Output was not 2, it was: " + str(result)
 
-    connectingRoutes, stopsToRoutes = solution.route_graph(routes)
-    print(connectingRoutes)
-    print(stopsToRoutes)
+    adjList = solution.route_graph(routes)
+    print(adjList)
