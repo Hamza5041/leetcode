c116f5e0e5cb5728fe79f26217a3c616e9ca7bac
Hamza Ansari <hamzaans@buffalo.edu>
2024-12-29 16:31:39 -0500

    fixed bfs to only increment bus count after proecessing the entire route. Also added a check if source/target are not in the adjList

--- hard/bus_routes.py
+++ hard/bus_routes.py
@@ -4,7 +4,15 @@ from typing import List
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
 
+        if source == target:
+            return 0
+
+
+
         adjList = self.route_graph(routes)
+
+        if source not in adjList or target not in adjList:
+            return -1
 
         queue = Queue()
         visited = set()
@@ -17,19 +25,21 @@ class Solution:
         buses = 1
         # BFS
         while not queue.empty():
-            # deque the current route ready in the queue
-            currentRoute = queue.get()
-            # check the routes array for stops. If the stop is the target, return the buses it took to get there
-            for stop in routes[currentRoute]:
-                if stop == target:
-                    return buses
-                # otherwise, queue all the routes for each stop into the queue and mark as visited if they have not been
-                for nextRoute in adjList.get(stop):
-                    if nextRoute not in visited:
-                        visited.add(nextRoute)
-                        queue.put(nextRoute)
+            for _ in range(queue.qsize()):
+                # deque the current route ready in the queue
+                currentRoute = queue.get()
+                # check the routes array for stops. If the stop is the target, return the buses it took to get there
+                for stop in routes[currentRoute]:
+                    if stop == target:
+                        return buses
+                    # otherwise, queue all the routes for each stop into the queue and mark as visited if they have not been
+                    for nextRoute in adjList.get(stop):
+                        if nextRoute not in visited:
+                            visited.add(nextRoute)
+                            queue.put(nextRoute)
             # increment buses taken at each iteration
             buses += 1
+
         return -1
 
 
@@ -49,13 +59,13 @@ class Solution:
 
 
 if __name__ == '__main__':
-    routes = [[1,2,7],[3,6,7]]
-    source = 1
-    target =6
+    routes = [[1,9,12,20,23,24,35,38],[10,21,24,31,32,34,37,38,43],[10,19,28,37],[8],[14,19],[11,17,23,31,41,43,44],[21,26,29,33],[5,11,33,41],[4,5,8,9,24,44]]
+    source = 37
+    target =28
 
     solution = Solution()
     result = solution.numBusesToDestination(routes, source, target)
-    assert result == 2, "Output was not 2, it was: " + str(result)
+    assert result == 1, "Output was not 1, it was: " + str(result)
 
     adjList = solution.route_graph(routes)
     print(adjList)
