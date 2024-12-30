8270c474bdcb6b7a15578947afcffbaf26b5f89b
Hamza Ansari <hamzaans@buffalo.edu>
2024-12-30 16:15:59 -0500

    working for simple version. Does not account for equals

--- medium/merge_intervals.py
+++ medium/merge_intervals.py
@@ -4,7 +4,16 @@ from typing import List
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x: x[0])
-        print(intervals)
+
+        end = float('-inf')
+        merged = [intervals[0]]
+        for i in range(1, len(intervals)):
+            print(i)
+            if intervals[i][0] < merged[-1][1]:
+                merged[-1][1] = intervals[i][1]
+            else:
+                merged.append(intervals[i])
+        return merged
 
 
 
@@ -16,4 +25,5 @@ if __name__ == '__main__':
     expected = [[1,6],[8,10],[15,18]]
 
     result = Solution().merge(intervals)
+    print("\nresult: " + str(result) + "\n" + "Expected: " + str(expected))
     # assert result == intervals, "\nresult: " + str(result) + "\n" + "Expected: " + str(expected)
