7f3969fa25c67afc5864bc5cf5b82a08de6cae2e
Hamza Ansari <hamzaans@buffalo.edu>
2024-12-30 15:21:04 -0500

    sorting array based on start times

--- medium/merge_intervals.py
+++ medium/merge_intervals.py
@@ -3,7 +3,10 @@ from typing import List
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        pass
+        intervals.sort(key=lambda x: x[0])
+        print(intervals)
+
+
 
 
 
@@ -13,4 +16,4 @@ if __name__ == '__main__':
     expected = [[1,6],[8,10],[15,18]]
 
     result = Solution().merge(intervals)
-    assert result == intervals, "\nresult: " + str(result) + "\n" + "Expected: " + str(expected)
+    # assert result == intervals, "\nresult: " + str(result) + "\n" + "Expected: " + str(expected)
