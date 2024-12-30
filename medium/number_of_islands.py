6182610b5167f0b725dd9c30c4a3d21795c703d2
Hamza Ansari <hamzaans@buffalo.edu>
2024-12-30 18:38:25 -0500

    changed it to have the grid iteself track visited nodes instead of a set. Much faster than before, now beats ~50% of others

--- medium/number_of_islands.py
+++ medium/number_of_islands.py
@@ -1,38 +1,37 @@
+from collections import deque
 from typing import List
-from queue import Queue
 
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         # standard bfs, checks for connected components
         numOfIslands = 0
-        queue = Queue()
-        visited = set()
+        # directions to check
+        directions = [ [1,0], [-1,0], [0,1], [0,-1]]
         rows, columns = len(grid), len(grid[0])
 
         # need to do bfs on each node for connected components
-        # every iteraction of the while loop means a new connected component (island
+        # every iteraction of the while loop means a new connected component (island)
         for r in range(rows):
             for c in range(columns):
-                if grid[r][c] == "1" and (r,c) not in visited:
+                # found start of next connected component
+                if grid[r][c] == "1":
                     numOfIslands += 1
-                    # add the first node to queue and visited
-                    queue.put((r, c))
-                    visited.add((r, c))
+                    # initialize the queue and add the first node found
+                    queue = deque([(r,c)])
                     # main bfs
-                    while not queue.empty():
-                        # pop off next row and column to explore
-                        currentRow, currentColumn = queue.get()
-                        # directions to check
-                        directions = [ [1,0], [-1,0], [0,1], [0,-1]]
-                        for dr,dc in directions:
-                            nextRow, nextColum = currentRow + dr, currentColumn + dc
-                            # check if  the nextRow and next column is within the range of rows and columns
-                            # then check if it is an valid land aka == 1
-                            # then check if it has been visited or not
-                            if 0 <= nextRow < rows and 0 <= nextColum < columns and grid[nextRow][nextColum] == "1" and (nextRow, nextColum) not in visited:
-                                queue.put((nextRow, nextColum))
-                                visited.add((nextRow,nextColum))
+                    while queue:
+                        # pop off row and column from queue
+                        currentRow, currentColumn = queue.popleft()
+                        # if the row and column is valid (within range), and it is land (==1) explore it
+                        if 0 <= currentRow < rows and 0 <= currentColumn < columns and grid[currentRow][currentColumn] == "1":
+                            # instead of using a set, mark the grid directly to keep track
+                            grid[currentRow][currentColumn] = "0"
+                            # add neighbors to be explored
+                            for dr,dc in directions:
+                                nextRow, nextColum = currentRow + dr, currentColumn + dc
+                                queue.append((nextRow, nextColum))
+
 
 
 
